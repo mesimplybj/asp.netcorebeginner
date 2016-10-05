a01b940cb1c560dae40af9c7cea750390a2243b1
bijay baniya <mt_bij@hotmail.com>
2016-10-05 22:36:58 +0545

    euler One solution

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ConsoleApplication
 {
     public class Program
@@ -7,6 +7,44 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            EulerOne();
+        }        
+
+        public static void EulerOne()
+        {
+            int x= int.Parse(Console.ReadLine());
+            int[] dividerArray = {3,5};
+            int dividerLength = dividerArray.Length;
+            int sum = 0;
+            List<int> dividerColletion = new List<int>();
+            for(int i =0 ; i < dividerLength;i++)
+            {
+                var divider = dividerArray[i];
+                for(int k=divider ; k < x ; k  += divider)
+                {
+                    if(NotIncluded(dividerColletion,k))
+                    {
+                        dividerColletion.Add(k);
+                        sum += k;
+                    }                    
+                }
+            }
+            Console.WriteLine(sum);
         }
-    }
+
+        public static bool NotIncluded(List<int> collections, int number)
+        {
+
+            bool notIncluded = true;
+            int collectionLength = collections.Count;
+            for(int i=0; i < collectionLength; i++)
+            {
+                    if(collections[i] == number)
+                    {
+                        notIncluded = false;    
+                    }
+            }
+            return notIncluded;
+        }
+    }    
 }
